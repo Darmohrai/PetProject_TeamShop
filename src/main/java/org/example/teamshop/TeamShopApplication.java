33d000185a7130d6d4013b59dd32904629c2b11f
volodymyrDobry <volodymyr.dobrianskyi1@gmail.com>
2025-01-02 15:07:11 +0300

    Project initialization

---
+++ src/main/java/org/example/teamshop/TeamShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.teamshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/teamshop/TeamShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.teamshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
